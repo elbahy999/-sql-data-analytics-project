4e50b8e488eb6ea6b4b41e2a994942f38eb0bac9
elbahy999 <118315573+elbahy999@users.noreply.github.com>
2025-09-06 17:52:15 +0300

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -13,7 +13,7 @@ cst_firstname NVARCHAR(50),
 cst_lastname NVARCHAR(50),
 cst_marital_status NVARCHAR(50),
 cst_gndr NVARCHAR(50),
-cst_create_date DATETIME
+cst_create_date DATE
 );
 
 CREATE TABLE bronze.crm_prd_info
@@ -23,7 +23,7 @@ prd_key NVARCHAR(50),
 prd_nm NVARCHAR(50),
 prd_cost INT,
 prd_line NVARCHAR(50),
-prd_start_dt DATE,
+prd_start_dt DATETIME,
 prd_end_dt DATETIME
 );
 
